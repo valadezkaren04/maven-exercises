b3c72329642c0a63cc14362aa22d65894c53f3d8
Karen Valadez <valadez.karen04@gmail.com>
2021-06-14 17:07:26 -0500

    added gitignore

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void sayHello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
---
+++ src/main/java/HelloWorldTest.java
@@ -0,0 +1,25 @@
+import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
+public class HelloWorldTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number...");
+        String userInput = scanner.nextLine();
+
+
+        if (!StringUtils.isNumeric(userInput)) {
+            System.out.println("\"" + userInput + "\" is not a number");
+        } else {
+            System.out.println(userInput);
+        }
+
+
+        // Flips the case of the string
+        System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
+
+        // Reverses the string
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+
+    }
+}
